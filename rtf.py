5aa04d2579f9aa321a211a966bdcec497ae81300
visacardshawty <marc.alwan9@gmail.com>
2024-02-20 10:42:23 +0200

    Retreived body types

--- rtf.py
+++ rtf.py
@@ -4,20 +4,25 @@ import json
 df = pd.read_csv('Australian Vehicle Prices.csv')
 brand_model_df = df[['Brand', 'Model']]
 unique_tuples = brand_model_df.drop_duplicates()
-print(unique_tuples)
 
-for_json = dict()
+bmjd = dict()
 for _, row in unique_tuples.iterrows():
     brand = row['Brand']
     model = row['Model']
-    if brand in for_json:
-        for_json[brand].add(model)
+    if brand in bmjd:
+        bmjd[brand].add(model)
     else:
-        for_json[brand] = {model}
+        bmjd[brand] = {model}
 
-for brand, models in for_json.items():
-    for_json[brand] = list(models)
-     
-json_data = json.dumps(for_json , indent=4)
+for brand, models in bmjd.items():
+    bmjd[brand] = list(models)
+ 
+type_df = df['BodyType']
+unique_types = type_df.drop_duplicates()  
+
+tjd = unique_types.tolist()
+print(tjd)
+        
+json_data = json.dumps({"Brand-Model":bmjd , "BodyType":tjd} , indent=4)
 with open('data_labels.json', 'w') as json_file:
     json_file.write(json_data)
